041d8062f78d105972635858cd726979999bd126
m-zaink <sidsmith9@gmail.com>
2019-06-23 15:14:58 +0530

    Make initial set - up | FC

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ruhm Farmer
+
+
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(RuhmFarmer());
+
+class RuhmFarmer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(),
+    );
+  }
+}
