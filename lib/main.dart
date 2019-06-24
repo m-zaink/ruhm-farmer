b25cda857ec599eaa0b057a7b5969ba2cc87eee3
m-zaink <sidsmith9@gmail.com>
2019-06-24 13:46:09 +0530

    Implement main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 
+import 'package:ruhm_farmer/screens/customer_screen.dart';
+import 'package:ruhm_farmer/screens/customers_screen.dart';
+import 'package:ruhm_farmer/screens/home_screen.dart';
+import 'package:ruhm_farmer/screens/log_in_screen.dart';
+
 void main() => runApp(RuhmFarmer());
 
 class RuhmFarmer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(),
+      theme: ThemeData.light().copyWith(
+        accentColor: Color(0xFF08D9D6),
+        highlightColor: Color(0xFF08D9D6),
+      ),
+      initialRoute: LogInScreen.logInScreenRoute,
+//      initialRoute: HomeScreen.homeScreenRoute,
+//      initialRoute: CustomerScreen.customerScreenRoute,
+//      initialRoute: CustomerScreen.customerScreenRoute,
+      routes: {
+        HomeScreen.homeScreenRoute: (context) => HomeScreen(),
+        LogInScreen.logInScreenRoute: (context) => LogInScreen(),
+        CustomersScreen.customersScreenRoute: (context) => CustomersScreen(),
+        CustomerScreen.customerScreenRoute: (context) => CustomerScreen(),
+      },
     );
   }
 }
